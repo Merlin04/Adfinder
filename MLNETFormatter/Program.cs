40a5978ba5b658af40309a8efbe7a17ba84516d3
merlin04 <Merlin04@users.noreply.github.com>
2020-03-19 17:35:32 -0700

    Remove line breaks

--- MLNETFormatter/Program.cs
+++ MLNETFormatter/Program.cs
@@ -21,7 +21,7 @@ namespace MLNETFormatter
             
             foreach (KeyValuePair<string, string> filePath in fm.GetFilesList())
             {
-                file.WriteLine(filePath.Value + "\t" + fm.GetArticleExtracts(filePath.Key));
+                file.WriteLine(filePath.Value + "\t" + fm.GetArticleExtracts(filePath.Key).Replace("\n", string.Empty));
             }
         }
     }
